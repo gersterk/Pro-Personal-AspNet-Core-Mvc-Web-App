3006c7c1d45709bcefb0d03f71d40ddb99013a92
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-13 14:55:03 +0200

    IGenericDal methods are setted

--- DataAccessLayer/Abstract/IGenericDal.cs
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    internal interface IGenericDal
+    public interface IGenericDal<T> where T : class
     {
+        void Insert(T t);
+        void Update(T t);
+        void Delete(T t);
+        List<T> GetAllList(); //if parameter needed, linq expression should be used with a filter
+
+        T GetById(int id);
+
+            
+        
+
     }
+
 }
