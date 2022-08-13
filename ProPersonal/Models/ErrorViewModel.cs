6c7bfec7d7080e19b40d9f18f96b2bf163b52758
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-13 10:12:48 +0200

    Add project files.

---
+++ ProPersonal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProPersonal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
