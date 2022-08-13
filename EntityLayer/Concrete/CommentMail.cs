6edc43261189994d49c86a9461e71d909fdc68c0
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-13 11:50:55 +0200

    EntityFramework packages are added. Key attributes are assigned to the id of each property of entities in EntityLayer

--- EntityLayer/Concrete/CommentMail.cs
+++ EntityLayer/Concrete/CommentMail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace EntityLayer.Concrete
 {
     public class CommentMail
     {
+        [Key]
         public int CommentId { get; set; }
         public string CommentUserName { get; set; }
         public string CommentContent { get; set; } 
--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace EntityLayer.Concrete
 {
     public class Writer
     {
+        [Key]
         public int WriterId { get; set; }
         public string WriterName { get; set; }
         public string WriteAbout { get; set; }
