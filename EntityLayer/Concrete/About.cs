2e8cdb03ac216e6046aa21f95265dc3719311b6c
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-13 11:36:04 +0200

    Entity Declaration: About and Contact

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        public int AbooutId { get; set; }
+        public string AboutDetails1 { get; set; }
+        public string AboutDetails2 { get; set; }
+        public string AboutImage1 { get; set; }
+        public string AboutImage2 { get; set; }
+
+        public string AboutMapLocation { get; set; }
+        //here I just want to try to use Map api and some frames. 
+        //nothing speacial. will see if its gonna work out
+        public bool IsActiveAbout { get; set; }
+
+    }
+}
