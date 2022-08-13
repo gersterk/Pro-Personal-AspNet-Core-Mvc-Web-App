6840364d06f1d7dacd205db4b9a123dd424dc37f
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-13 10:50:26 +0200

    EntityLayer/Category Entity is added...

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//This Entity as a class represents the table in our database
+//if this be so the properties are the columns
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public string CategoryDescription { get; set; }
+        public bool CategoryIsActive { get; set; }
+    } 
+}
+
