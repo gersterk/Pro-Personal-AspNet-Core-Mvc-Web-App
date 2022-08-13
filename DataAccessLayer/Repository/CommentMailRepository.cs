36e40823e992bad750ed86b5563d991af4087cfd
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-13 15:04:50 +0200

    Generic Repo

---
+++ DataAccessLayer/Repository/CommentMailRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class CommentMailRepository : IGenericDal<CommentMail>
+    {
+        public void Delete(CommentMail t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CommentMail> GetAllList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CommentMail GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(CommentMail t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(CommentMail t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
