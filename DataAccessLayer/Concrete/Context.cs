e7ed34c9479bd6c532cf3214ce3c5647eddd6d11
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-13 12:24:27 +0200

    Sql server connection builder is set, solution guides added. Project references are added.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+
+
+    //contexts are inheriting from DbContext class that EnityFramework Core library prodives
+    
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {   
+
+            //Building Database
+            optionsBuilder.UseSqlServer("server = DESKTOP-TLTN1FO;database = ProPersonalDb; integrated security=true");
+
+
+        }
+    }
+}
