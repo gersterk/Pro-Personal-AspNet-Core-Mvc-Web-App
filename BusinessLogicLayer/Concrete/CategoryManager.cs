8200c25aef4a09feb9d921d947bed7fd1ec07993
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-13 15:53:00 +0200

    Genericize the interfaces

---
+++ BusinessLogicLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLogicLayer.Abstract;
+using DataAccessLayer.GenericRepository;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+
+        public void CategoryAdd(Category category)
+        {
+            throw new NotImplementedException();
+
+
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
