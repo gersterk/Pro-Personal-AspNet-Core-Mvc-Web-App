df51dd97576b99be6e1ae0211ed8e9bc5243a2de
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-14 14:23:14 +0200

    CommentListByBlog via ViewComponents

--- ProPersonal/Models/ClientComment.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProPersonal.Models
-{
-    public class ClientComment
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-    }
-}
---
+++ ProPersonal/ViewComponents/CommentComponents/CommentListByBlog.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProPersonal.ViewComponents.CommentComponents
+{
+    public class CommentListByBlog : ViewComponent
+    {
+        CommentMailManager cm = new CommentMailManager(new EfCommentMailRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList(4);
+
+            return View(values);
+        }
+    }
+}
