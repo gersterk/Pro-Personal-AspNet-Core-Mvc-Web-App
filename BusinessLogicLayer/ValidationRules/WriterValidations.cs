ca1a5cf440dd7ae816a383e0cc529379912301b6
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-14 22:12:41 +0200

    Validation Rules for Writer to Register

---
+++ BusinessLogicLayer/ValidationRules/WriterValidations.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.ValidationRules
+{
+    public class WriterValidations :AbstractValidator<Writer>
+    {
+        public WriterValidations()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("You'd better don't pass here by");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("How would I mail you?");
+            RuleFor(x=>x.WriterPassword).NotEmpty().WithMessage("Dude! That is not very cool!")
+                .MinimumLength(8).WithMessage("Passwords are usually longer than 8 characters.")
+                .MaximumLength(16).WithMessage("Passwords are usually not longer than 16 characters.")
+                .Matches(@"[A-Z]+").WithMessage("At least one upper letter?")
+                .Matches(@"[a-z]+").WithMessage("At least one lower letter?")
+                .Matches(@"[0-9]+").WithMessage("What about spicing up your pass with some cool numbers?");
+        }
+    }
+}
--- ProPersonal/Controllers/RegisterController.cs
+++ ProPersonal/Controllers/RegisterController.cs
@@ -1,7 +1,10 @@
 ﻿using BusinessLogicLayer.Concrete;
+using BusinessLogicLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace ProPersonal.Controllers
 {
@@ -21,10 +24,26 @@ namespace ProPersonal.Controllers
         [HttpPost]
         public IActionResult Index(Writer p)
         {
-            p.IsActiveWriter = true;
-            p.WriteAbout = "Deneme";
-            wm.WriterAdd(p);    
-            return RedirectToAction("Index", "Blog");
+            WriterValidations wv = new WriterValidations();
+            ValidationResult results = wv.Validate(p);
+            if (results.IsValid)
+            {
+
+                p.IsActiveWriter = true;
+                p.WriteAbout = "Deneme";
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+
+            }
+            else
+            {
+                foreach(var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
 
         }
     }
