225b7bae862edcecd6427700ded31664c88d7ee9
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-14 21:33:00 +0200

    Register Section is on going...

---
+++ BusinessLogicLayer/Concrete/WriterManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLogicLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Concrete
+{
+    public class WriterManager: IWriterService
+    {
+        IWriterDal _writerDal;
+
+        public WriterManager(IWriterDal writerDal)
+        {
+            _writerDal = writerDal;
+        }
+
+        public void WriterAdd(Writer writer)
+        {
+            _writerDal.Insert(writer);
+        }
+    }
+}
---
+++ ProPersonal/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProPersonal.Controllers
+{
+    
+    public class RegisterController : Controller
+    {
+
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer p)
+        {
+            p.IsActiveWriter = true;
+            p.WriteAbout = "Deneme";
+            wm.WriterAdd(p);    
+            return RedirectToAction("Index", "Blog");
+
+        }
+    }
+}
