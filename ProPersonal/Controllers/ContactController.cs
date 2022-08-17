9d414c63e8a62187bd5ac29e58314fefeb14eff1
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-17 19:40:49 +0200

    Contact session

---
+++ ProPersonal/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ProPersonal.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact p)
+        {
+            p.ContactDate = DateTime.Parse(DateTime.Now.ToLongDateString());
+            p.IsActiveContact = true;
+            cm.ContactAdd(p);
+
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
