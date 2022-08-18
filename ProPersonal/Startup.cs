4365340797711c2b63ba5d1bddcdcbbef2ac1836
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-18 19:22:06 +0200

    authorization fails for directing to the login

--- ProPersonal/Startup.cs
+++ ProPersonal/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -37,6 +38,15 @@ namespace ProPersonal
 
                 config.Filters.Add(new AuthorizeFilter(policy));
             });
+
+            services.AddMvc();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/Login/Index";    //returns url and avoids scheme error when theres no authentication but a client wants to at least register
+
+                });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
