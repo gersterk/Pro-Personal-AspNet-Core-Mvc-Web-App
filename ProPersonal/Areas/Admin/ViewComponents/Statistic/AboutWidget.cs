7592d1f80c1af57fb33f0e0e1660699fb76df9a9
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-26 23:07:02 +0200

    About, Last Blogs widgets

---
+++ ProPersonal/Areas/Admin/ViewComponents/Statistic/AboutWidget.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ProPersonal.Areas.Admin.ViewComponents.Statistic
+{
+    
+    public class AboutWidget : ViewComponent
+    {
+
+        Context c = new Context();
+
+    
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.lastBlog = c.Blogs.OrderByDescending(x=>x.BlogId).Select(x => x.BlogName).Take(1).FirstOrDefault();
+
+            return View();
+
+        }
+    }
+}
