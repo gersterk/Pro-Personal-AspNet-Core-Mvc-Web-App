a0d7f42aab9e198d27c8189c57e1bcd4fc7aba65
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-26 22:33:22 +0200

    Widgets and Statistics, total blog and comment, messages

---
+++ ProPersonal/Areas/Admin/ViewComponents/Statistic/TotalBlogStatistic.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ProPersonal.Areas.Admin.ViewComponents.Statistic
+{
+    public class TotalBlogStatistic : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context();
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.blogTotal = bm.GetList().Count();
+            ViewBag.commentTotal = c.CommentMails.Count();
+
+            return View();  
+
+        }
+    }
+}
