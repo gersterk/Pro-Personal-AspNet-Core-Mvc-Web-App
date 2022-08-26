51142671c9feed48364b8950caa68d81ddabba90
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-26 21:07:34 +0200

    Dynamic Excel Files by sessions

--- ProPersonal/Areas/Admin/Controllers/BlogController.cs
+++ ProPersonal/Areas/Admin/Controllers/BlogController.cs
@@ -1,8 +1,10 @@
 ﻿using ClosedXML.Excel;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using ProPersonal.Areas.Admin.Models;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace ProPersonal.Areas.Admin.Controllers
 {
@@ -13,7 +15,7 @@ namespace ProPersonal.Areas.Admin.Controllers
         {
             using(var workbook = new XLWorkbook())
             {
-                var worksheet = workbook.Worksheets.Add("Blog Lists");
+                var worksheet = workbook.Worksheets.Add("Blogs List");
                 worksheet.Cell(1, 1).Value = "Blog Id";
                 worksheet.Cell(1, 2).Value = "Blog Name";
 
@@ -51,6 +53,60 @@ namespace ProPersonal.Areas.Admin.Controllers
         }
 
         public IActionResult BlogListExcel()
+        {
+            return View();
+
+        }
+
+        //the methods above were a try get the names statically... even though I know I could be confusing, I will not delete them :)
+        //I couldnt comprehend yet xd
+
+        public IActionResult ExportDynamicExcelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blogs List");
+                worksheet.Cell(1, 1).Value = "Blog Id";
+                worksheet.Cell(1, 2).Value = "Blog Name";
+
+
+                int blogRowCount = 2;
+                foreach (var item in BlogListExcelDynamic())
+                {
+                    worksheet.Cell(blogRowCount, 1).Value = item.Id;
+                    worksheet.Cell(blogRowCount, 2).Value = item.BlogTitle;
+                    blogRowCount++;
+
+
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Blogs.xlsx");
+
+                }
+
+            }
+        }
+
+        public List<BlogModel2> BlogListExcelDynamic()
+        {
+            List<BlogModel2> bm = new List<BlogModel2>();
+            using(var c = new Context())
+            {
+                bm = c.Blogs.Select(x => new BlogModel2
+                {
+                    Id = x.BlogId,
+                    BlogTitle = x.BlogName
+                    
+                }).ToList();
+
+            }
+            return bm;
+
+        }
+        public IActionResult BlogTitleListExcel()
         {
             return View();
 
---
+++ ProPersonal/Areas/Admin/Models/BlogModel2.cs
@@ -0,0 +1,8 @@
+﻿namespace ProPersonal.Areas.Admin.Models
+{
+    public class BlogModel2
+    {
+        public int Id { get; set; }
+        public string BlogTitle { get; set; }
+    }
+}
