393eea4cf471a21b71e79e452645a93402a22f3a
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-26 20:52:11 +0200

    Exporting an Excel file of the list of blogs

---
+++ ProPersonal/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,59 @@
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
+using ProPersonal.Areas.Admin.Models;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ProPersonal.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        public IActionResult ExportStaticExcelBlogList()
+        {
+            using(var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Lists");
+                worksheet.Cell(1, 1).Value = "Blog Id";
+                worksheet.Cell(1, 2).Value = "Blog Name";
+
+
+                int blogRowCount = 2;
+                foreach (var item in GetBlogList())
+                {
+                    worksheet.Cell(blogRowCount, 1).Value = item.Id;
+                    worksheet.Cell(blogRowCount, 2).Value = item.BlogTitle;
+                    blogRowCount++;
+
+
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Blogs.xlsx");
+
+                }
+
+            }
+        }
+
+        public List<BlogModel> GetBlogList()
+        {
+            List<BlogModel> bm = new List<BlogModel>
+            {
+                new BlogModel{Id=1, BlogTitle="Test Row"}, new BlogModel{Id=2, BlogTitle="testtt"}
+
+
+            };
+            return bm;
+
+        }
+
+        public IActionResult BlogListExcel()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ ProPersonal/Areas/Admin/Models/BlogModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProPersonal.Areas.Admin.Models
+{
+    public class BlogModel
+    {
+        public int Id { get; set; }
+        public string BlogTitle { get; set; }
+    }
+}
