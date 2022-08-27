974c61ecfe7cd939c59a627a0ff896ab7b2127db
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-27 11:13:28 +0200

    Charts, I hate JS

---
+++ ProPersonal/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProPersonal.Areas.Admin.Models;
+using System.Collections.Generic;
+
+namespace ProPersonal.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart() //static chart
+        {
+            List<CategoryModel> listCategory = new List<CategoryModel>();
+            listCategory.Add(new CategoryModel
+            {
+                CategoryCount = 10,
+                CategoryName = "Technology",
+            });
+            listCategory.Add(new CategoryModel
+            {
+                CategoryCount = 18,
+                CategoryName = "Software",
+            });
+            listCategory.Add(new CategoryModel
+            {
+                CategoryCount = 4,
+                CategoryName = "Philosophy",
+            });
+
+            return Json(new {jsonlist=listCategory});
+
+
+        }
+    }
+}
---
+++ ProPersonal/Areas/Admin/Models/CategoryModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProPersonal.Areas.Admin.Models
+{
+    public class CategoryModel
+    {
+        public int CategoryCount { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
