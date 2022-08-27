653ff9c1b68ac9f5ab683c12be7aac7a1b653b72
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-27 09:55:48 +0200

    Weather Api

--- ProPersonal/Areas/Admin/ViewComponents/Statistic/TotalBlogStatistic.cs
+++ ProPersonal/Areas/Admin/ViewComponents/Statistic/TotalBlogStatistic.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace ProPersonal.Areas.Admin.ViewComponents.Statistic
 {
@@ -15,6 +16,11 @@ namespace ProPersonal.Areas.Admin.ViewComponents.Statistic
         {
             ViewBag.blogTotal = bm.GetList().Count();
             ViewBag.commentTotal = c.CommentMails.Count();
+
+            string api = "39a5fb44446ea15aaaf17aa90fd018fa";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=bratislava&mode=xml&lang=eng&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);  
+            ViewBag.weatherInfo = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
 
             return View();  
 
