438a161b060ec6ee606babaf298696b802c7d6cd
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-28 13:02:36 +0200

    User Login

---
+++ ProPersonal/Models/UserLoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProPersonal.Models
+{
+    public class UserLoginModel
+    {
+
+       
+        [Required(ErrorMessage = "Please input your mail!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please input your password!")]
+        public string UserPassword { get; set; }
+    }
+}
