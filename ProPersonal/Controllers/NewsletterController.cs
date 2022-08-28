ac56577613a2224f45c6ad168bc5d6c6b0109330
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-28 13:36:02 +0200

    Subscribe mail bulletin AJAX

--- ProPersonal/Controllers/NewsletterController.cs
+++ ProPersonal/Controllers/NewsletterController.cs
@@ -1,10 +1,12 @@
 ﻿using BusinessLogicLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ProPersonal.Controllers
 {
+    [AllowAnonymous]
     public class NewsletterController : Controller
     {
         NewsletterManager nm = new NewsletterManager(new EfNewsletterRepository());
