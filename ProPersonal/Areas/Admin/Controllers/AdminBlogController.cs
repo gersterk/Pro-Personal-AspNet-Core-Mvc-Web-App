9ced48d84e96be91eacde52befc798ca4c27c633
Baris Dogan <barisdogan.biz@gmail.com>
2022-08-29 10:43:54 +0200

    Blog Listing by category and writer in admin dashboard

---
+++ ProPersonal/Areas/Admin/Controllers/AdminBlogController.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProPersonal.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBlogController : Controller
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+
+        public IActionResult Index()
+        {
+            var blogGetAll = blogManager.GetBlogListByCategory();
+            return View(blogGetAll);
+        }
+    }
+}
