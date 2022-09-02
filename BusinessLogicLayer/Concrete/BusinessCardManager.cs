e14bf663e434a12b310ca3a1bc3f47d4db5f511d
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-02 19:49:20 +0200

    BusinessCard DAL, BLL dependencies

---
+++ BusinessLogicLayer/Concrete/BusinessCardManager.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLogicLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Concrete
+{
+    public class BusinessCardManager : IBusinessCardService
+    {
+        public List<BusinessCard> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(BusinessCard t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(BusinessCard t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BusinessCard TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(BusinessCard t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
