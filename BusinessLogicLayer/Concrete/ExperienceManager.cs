38716df41c010f9c76b7484a8acf9840148b1e77
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-03 12:37:24 +0200

    Experience Architecture

---
+++ BusinessLogicLayer/Concrete/ExperienceManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLogicLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Concrete
+{
+    public class ExperienceManager : IExperienceService
+    {
+        IExperienceDal _experienceDal;
+
+        public ExperienceManager(IExperienceDal experienceDal)
+        {
+            _experienceDal = experienceDal;
+        }
+
+        public List<Experience> GetList()
+        {
+            return _experienceDal.GetAllList();
+
+        }
+
+        public void TAdd(Experience t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Experience t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Experience TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Experience t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
