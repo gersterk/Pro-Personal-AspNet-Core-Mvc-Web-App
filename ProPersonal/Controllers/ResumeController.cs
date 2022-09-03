9b1580e193c10718ad165be9dae94388fd28877e
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-03 10:54:20 +0200

    --

--- ProPersonal/Controllers/ResumeController.cs
+++ ProPersonal/Controllers/ResumeController.cs
@@ -1,34 +1,17 @@
 ﻿using BusinessLogicLayer.Concrete;
-using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
 
 namespace ProPersonal.Controllers
 {
     public class ResumeController : Controller
     {
-        BusinessCardManager bcm = new BusinessCardManager(new EfBusinessCardRepository());
-        SkillManager skillManager = new SkillManager(new EfSkillRepository());
-
-        [AllowAnonymous]
+       
         public IActionResult Index()
         {
+         
             return View();
         }
 
-        public IActionResult Card(int id)
-        {
-            var getCard = bcm.TGetById(id);
-            return View(getCard);
-        }
-
-        public IActionResult MySkills()
-        {
-            var values = skillManager.GetList(); //should come from SkillManager but theres no such yet... 
-            return View(values);
-
-        }
     }
 }
