0df15de86187aed5bffc040b1930d1cb4e9e8c3e
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-04 18:59:40 +0200

    Design Enhancement

--- ProPersonal/Controllers/AboutController.cs
+++ ProPersonal/Controllers/AboutController.cs
@@ -1,9 +1,11 @@
 ﻿using BusinessLogicLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ProPersonal.Controllers
 {
+    [AllowAnonymous]
     public class AboutController : Controller
     {
         AboutManager ab = new AboutManager(new EfAboutRepository());
