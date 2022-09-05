332ebd47bca38fa7a62a4d80aebc35a4292a2829
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-05 18:16:56 +0200

    text editor

--- ProPersonal/Controllers/BlogController.cs
+++ ProPersonal/Controllers/BlogController.cs
@@ -145,5 +145,7 @@ namespace ProPersonal.Controllers
 
         }
 
+
+
     }
 }
---
+++ ProPersonal/ViewComponents/Blog/TextEditor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProPersonal.ViewComponents
+{
+    public class TextEditor : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
