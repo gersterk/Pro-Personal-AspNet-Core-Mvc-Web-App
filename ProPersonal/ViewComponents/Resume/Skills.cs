979c4bc2f565f6cce8f393320cfaac3468b5357c
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-06 18:41:09 +0200

    skills in resume

---
+++ ProPersonal/ViewComponents/Resume/Skills.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProPersonal.ViewComponents.Resume
+{
+    public class Skills : ViewComponent
+    {
+        SkillManager skillManager = new SkillManager(new EfSkillRepository());
+
+        public IViewComponentResult Invoke ()
+        {
+            var getSkills = skillManager.GetList();
+            return View(getSkills);
+        }
+    }
+}
