e68e61b9e060aeabeec020fd6204a7e46e258aef
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-06 19:52:56 +0200

    Interests

---
+++ BusinessLogicLayer/Concrete/InterestManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLogicLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Concrete
+{
+    public class InterestManager : IInterestService
+    {
+        IInterestDal _interestDal;
+
+        public InterestManager(IInterestDal interestDal)
+        {
+            _interestDal = interestDal;
+        }
+
+        public List<Interest> GetList()
+        {
+            return _interestDal.GetAllList();
+        }
+
+        public void TAdd(Interest t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Interest t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Interest TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Interest t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProPersonal/ViewComponents/Resume/Interest.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProPersonal.ViewComponents.Resume
+{
+    public class Interest : ViewComponent
+    {
+        InterestManager interestManager = new InterestManager(new EfInterestRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
