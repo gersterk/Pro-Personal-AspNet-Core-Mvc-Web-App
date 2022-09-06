7728dbec51a553ec8a7f3fff6d9ddc454b2d7f19
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-06 20:12:22 +0200

    Experience

---
+++ ProPersonal/ViewComponents/Resume/Experience.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProPersonal.ViewComponents.Resume
+{
+    public class Experience : ViewComponent
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceRepository());
+        public IViewComponentResult Invoke()
+        {
+            var getList = experienceManager.GetList();
+            return View(getList);
+        }
+    }
+}
