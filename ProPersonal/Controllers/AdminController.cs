ece2dbe88f705a29dadde0cfb748ff657000ae75
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-12 20:11:13 +0200

    enhancements

--- ProPersonal/Controllers/AdminController.cs
+++ ProPersonal/Controllers/AdminController.cs
@@ -1,16 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ProPersonal.Controllers
 {
     public class AdminController : Controller
     {
+        WriterManager writerManager = new WriterManager (new EfWriterRepository());
         public IActionResult Index()
         {
             return View();
         }
 
-        public PartialViewResult AdminNavbarPartial()
+        public PartialViewResult AdminNavbarPartial(int id=1)
         {
+            var getWriters = writerManager.TGetById(id);
+            ViewBag.writerImage = getWriters.WriterImage;
             return PartialView();
 
         }
